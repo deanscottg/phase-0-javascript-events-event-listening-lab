bf09544b7ba0801cfb48731bc04ca059dc5c885e
Dean <deanscottg@gmail.com>
2022-01-16 15:39:11 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-function addingEventListener() {
+
+function addingEventListener(){
+    const input =document.getElementById('input');
+   input.addEventListener('click',function(){
+       alert('You are tearing me apart,Lisa!');
+   });
 }
