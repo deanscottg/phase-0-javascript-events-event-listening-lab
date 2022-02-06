a73f6ec5e3f1883afc1198ae351679e29d673872
Dean <deanscottg@gmail.com>
2022-02-06 17:15:48 -0500

    commit tree cce1d06d29d420ce1106e7babd5614489587de06

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 
+// function addingEventListener(){
+//     const input =document.getElementById('input');
+//    input.addEventListener('click',function(){
+//        alert('You are tearing me apart,Lisa!');
+//    });
+// }
+
+const input = document.querySelector('input');
+function addAlert(){
+    alert('You are tearing me apart, Lisa!')
+}
 function addingEventListener(){
-    const input =document.getElementById('input');
-   input.addEventListener('click',function(){
-       alert('You are tearing me apart,Lisa!');
-   });
+    input.addEventListener('click',addAlert)
 }
